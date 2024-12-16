fcd23ceca27ab80c9be1e87a99cc6816472e2bd9
Dimitri MOMPEROUSSE <momperousse.dimitri.pro@gmail.com>
2024-12-16 11:55:45 +0100

    first commit

---
+++ src/main/java/com/example/GameLibraryOnline2/GameLibraryOnline2Application.java
@@ -0,0 +1,14 @@
+package com.example.GameLibraryOnline2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryOnline2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibraryOnline2Application.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/com/example/GameLibraryOnline2/GameLibraryOnline2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GameLibraryOnline2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLibraryOnline2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
